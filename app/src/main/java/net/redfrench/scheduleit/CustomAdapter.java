56c49f77b1c0ba848340c1ff3ef5be481b472781
Red-French <red@redfrench.net>
2017-04-02 22:44:44 -0500

    experiment; not currently using

--- app/src/main/java/net/redfrench/scheduleit/CustomAdapter.java
+++ app/src/main/java/net/redfrench/scheduleit/CustomAdapter.java
@@ -1,8 +1,42 @@
-package net.redfrench.scheduleit;
-
-/**
- * Created by user on 4/2/17.
- */
-
-public class CustomAdapter {
-}
+//package net.redfrench.scheduleit;
+//
+//import android.content.Context;
+//import android.support.annotation.LayoutRes;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+//import android.text.Layout;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import static net.redfrench.scheduleit.R.id.timeslots;
+//
+//class CustomAdapter extends ArrayAdapter<String>{  // note: removed 'public'
+//
+//    public CustomAdapter(@NonNull Context context, @LayoutRes String[] timeslots) {  // pass timeSlots to constructor
+//        super(context, R.layout.custom_row, timeslots);
+//    }
+//
+//    @NonNull
+//    @Override  // set view of the time slot list
+//    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//
+//        // initialization
+//        LayoutInflater myInflater = LayoutInflater.from(getContext());
+//        View CustomView = myInflater.inflate(R.layout.custom_row, parent, false);
+//
+//        // get references
+//        String timePosition = getItem(position);  // get position reference of each item
+//        TextView myText = (TextView) CustomView.findViewById(R.id.myText);
+//        ImageView myImage = (ImageView) CustomView.findViewById(R.id.myImage);
+//
+//        // set text and image
+//        myText.setText(timePosition);
+//        myImage.setImageResource(android.R.drawable.ic_delete);
+//
+//        return CustomView;
+//    }
+//}
