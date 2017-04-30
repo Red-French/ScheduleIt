c8adcd926cffb619b99eb4f23e63f465f62fd8e9
Red-French <red@redfrench.net>
2017-04-29 20:01:24 -0500

    initial commit

---
+++ app/backend/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+  $hostname = "localhost";
+  $username = "id1157462_unclefatty";
+  $password = "000webhost2017";
+  $database = "id1157462_scheduler";
+
+ ?>
---
+++ app/src/main/java/net/redfrench/scheduleit/Services.java
@@ -0,0 +1,8 @@
+package net.redfrench.scheduleit;
+
+/**
+ * Created by user on 4/27/17.
+ */
+
+public class Services {
+}
